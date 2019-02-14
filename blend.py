fcb88123eab24f7dbb2af689e9f6d391d2f9563b
lin <1462243304@qq.com>
2019-02-14 22:15:04 +0800

    update code

---
+++ blend.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from utils.config import Config
+import os
+
+from scipy.stats import rankdata
+
+
+def blend_predictions(predict_list, one_prediction):
+    print("Rank averaging on ", len(predict_list), " files")
+    predictions = np.zeros_like(predict_list[0])
+    for predict in predict_list:
+        for i in range(1):
+            predictions[:, i] = np.add(predictions[:, i], rankdata(predict[:, i])/predictions.shape[0])
+    predictions /= len(predict_list)
+
+    one_prediction["HasDetections"] = predictions
+    one_prediction.to_csv('super_blend.csv', index=False)
+
+
+if __name__ == '__main__':
+    LABELS = ["HasDetections"]
+    predict_list = []
+    one_prediction = None
+    for file in os.listdir('data/blend'):
+        filepath = 'data/blend/' + file
+        one_prediction = pd.read_csv(filepath)
+        predict_list.append(one_prediction[LABELS].values)
+
+    blend_predictions(predict_list, one_prediction)
